3dc9d5f420c51238ac63785ca5ba4dbe490a9a7b
Kyle Thomas <kyle.thomas@ladbiblegroup.com>
2018-07-20 16:16:35 +0100

    Initial Commit

---
+++ Mugshot/Mugshot.h
@@ -0,0 +1,19 @@
+//
+//  Mugshot.h
+//  Mugshot
+//
+//  Created by Kyle Thomas on 20/07/2018.
+//  Copyright © 2018 Kyle Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mugshot.
+FOUNDATION_EXPORT double MugshotVersionNumber;
+
+//! Project version string for Mugshot.
+FOUNDATION_EXPORT const unsigned char MugshotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mugshot/PublicHeader.h>
+
+
